6292c660611cf85fecb3db4998107a1b70cf393c
danilo <danilocencigg@gmail.com>
2025-03-25 17:54:17 -0300

    Add project files.

---
+++ DiamanteDeXAP/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DiamanteDeXAP
+{
+    internal class Program
+    {
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        public static void Enquire()
+        {
+            Console.WriteLine("Programa Finalizado, deseja reiniciar? (y/n)");
+            string enquire = Console.ReadLine();
+            if (enquire == "y")
+            {
+
+            }
+
+
+        } 
+        
+        
+        
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
