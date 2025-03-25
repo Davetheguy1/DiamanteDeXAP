b57a4dd7b0bfc141c6108ec1a5fd263492001827
danilo <danilocencigg@gmail.com>
2025-03-25 18:12:12 -0300

    added enquire function

--- DiamanteDeXAP/Program.cs
+++ DiamanteDeXAP/Program.cs
@@ -15,14 +15,26 @@
         
         
         
+        
         public static void Enquire()
         {
             Console.WriteLine("Programa Finalizado, deseja reiniciar? (y/n)");
             string enquire = Console.ReadLine();
+
             if (enquire == "y")
             {
+                //Calls Previous Function Back
 
+            } else if (enquire == "n")
+            {
+                Console.WriteLine("Fechando Programa....");
+                Environment.Exit(0);
+            } else
+            {
+                Console.WriteLine("Erro, Favor Digitar uma Opção Válida");
+                Enquire();
             }
+            
 
 
         } 
