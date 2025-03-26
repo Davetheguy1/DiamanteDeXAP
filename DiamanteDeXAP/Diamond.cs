00608bc32e9f40d1dbf911423012883c42ff25d6
danilo <danilocencigg@gmail.com>
2025-03-26 13:03:05 -0300

    outra tentativa de tentar fazer o diamante

--- DiamanteDeXAP/Diamond.cs
+++ DiamanteDeXAP/Diamond.cs
@@ -28,27 +28,46 @@ namespace DiamanteDeXAP
         {
             int baseValue = Program.playerValue;
 
+            
+                string firstString = new string('X', baseValue);
+                string secondString = new string('X', (baseValue - 2));
+                string thirdString = new string('X', (baseValue - 4));
+                string fourthString = new string('X', (baseValue - 6));
+                string fifhtString = new string('X', (baseValue - 8));
+                string sixthString = new string('X', (baseValue - 10));
+                string seventhString = new string('X', baseValue - 12);
+                string eighthString = new string('X', baseValue - 14);
+                string ninthString = new string('X', baseValue - 16);
+                string tenthString = new string('X', baseValue - 18);
+                string eleventhString = new string('X', baseValue - 20);
 
-            string firstString = new string('X', baseValue);
+
+
+
+                Console.WriteLine("      {0}      ", eleventhString);
+                Console.WriteLine("      {0}      ", tenthString);
+                Console.WriteLine("      {0}      ", ninthString);
+                Console.WriteLine("      {0}      ", eighthString);
+                Console.WriteLine("      {0}      ", seventhString);
+                Console.WriteLine("      {0}      ", sixthString);
+                Console.WriteLine("      {0}      ", fifhtString);
+                Console.WriteLine("      {0}      ", fourthString);
+                Console.WriteLine("      {0}      ", thirdString);
+                Console.WriteLine("      {0}      ", secondString);
+                Console.WriteLine("      {0}      ", firstString);
+                Console.WriteLine("      {0}      ", secondString);
+                Console.WriteLine("      {0}      ", thirdString);
+                Console.WriteLine("      {0}      ", fourthString);
+                Console.WriteLine("      {0}      ", fifhtString);
+                Console.WriteLine("      {0}      ", sixthString);
+                Console.WriteLine("      {0}      ", seventhString);
+                Console.WriteLine("      {0}      ", eighthString);
+                Console.WriteLine("      {0}      ", ninthString);
+                Console.WriteLine("      {0}      ", tenthString);
+                Console.WriteLine("      {0}      ", eleventhString);
             
             
-            
-            
-            
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ", firstString);
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-            Console.WriteLine("     {0}       ");
-             
+             Program.Enquire();
 
         }
 
